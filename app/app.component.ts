4d6816d4037831f2b0ca8aeb7fcec147538c179e
jeonghwan-kim <ej88ej@gmail.com>
2016-08-25 10:59:30 +0900

    Init project by quickstart

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
-  template: '<hero-form></hero-form>'
+  template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
-import { HeroFormComponent } from './hero-form.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    AppComponent,
-    HeroFormComponent
-  ],
-  bootstrap: [ AppComponent ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
